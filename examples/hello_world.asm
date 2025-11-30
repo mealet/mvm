c7e2e6b94e8471dd5d406f4e50565ac80e2f5675
mealet <jasonragonter@gmail.com>
2025-11-30 19:19:59 +0300

    reviewed example

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -2,23 +2,29 @@
 ; %call = %r9
 ; $syscall = $0x80
 
-.data
+section .data
   hello:
-    .ascii "Hello, World!"
-  len = [. - &hello]
+    ascii "Hello, World!"
 
-.text
+  len:
+    [. - &hello]
+
+section .text
 
 _start:
   // -- syscall void write(int output, void *buffer, size_t length) --
 
   mov %r0, $0 // int output (stdout)
-  ldr %r1, &hello // void* buffer (ptr to message)
-  ldr %r2, len // size_t length (length variable)
+
+  mov %r1, &hello // void* buffer (ptr to message)
+  mov %r2, len // size_t length (length variable)
 
   mov %call, $2 // `write` syscall
   int $syscall // system call interrupt
 
+  jmp &exit
+
+exit:
   // -- syscall void exit(int status) --
   mov %r0 $0 // int status
   mov %call $0 // `exit` syscall
