13a935b565b743a84c886e898e06852f35194505
mealet <jasonragonter@gmail.com>
2025-11-30 21:48:53 +0300

    fixed example

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -12,20 +12,20 @@ section .data
 section .text
 
 _start:
-  // -- syscall void write(int output, void *buffer, size_t length) --
+  ; -- syscall void write(int output, void *buffer, size_t length) --
 
-  mov %r0, $0 // int output (stdout)
+  mov %r0, $0 ; int output (stdout)
 
-  mov %r1, &hello // void* buffer (ptr to message)
-  mov %r2, len // size_t length (length variable)
+  mov %r1, &hello ; void* buffer (ptr to message)
+  mov %r2, len ; size_t length (length variable)
 
-  mov %call, $2 // `write` syscall
-  int $syscall // system call interrupt
+  mov %call, $2 ; `write` syscall
+  int $syscall ; system call interrupt
 
   jmp &exit
 
 exit:
-  // -- syscall void exit(int status) --
-  mov %r0 $0 // int status
-  mov %call $0 // `exit` syscall
-  int $syscall // system call interrupt
+  ; -- syscall void exit(int status) --
+  mov %r0 $0 ; int status
+  mov %call $0 ; `exit` syscall
+  int $syscall ; system call interrupt
