380b1da4fc06dd21290d33efa81255ea1d4a81d6
mealet <jasonragonter@gmail.com>
2025-11-30 21:23:36 +0300

    added run function for vm

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -90,12 +90,15 @@ impl VM {
         Ok(())
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> Result<(), MvmError> {
         self.running = true;
 
         while self.running {
-
+            let instruction = self.fetch_u8()?;
+            self.execute_instruction(instruction);
         }
+
+        Ok(())
     }
 }
 
@@ -139,6 +142,62 @@ impl VM {
 
 impl VM {
     fn execute_instruction(&mut self, instruction: u8) -> Result<(), MvmError> {
+        let opcode = Opcode::try_from(instruction)?;
+
+        match opcode {
+            Opcode::Halt => {
+                self.running = false;
+            },
+            Opcode::Return => todo!(),
+            Opcode::Call => todo!(),
+            Opcode::Interrupt => todo!(),
+
+            Opcode::DataSection => todo!(),
+            Opcode::TextSection => todo!(),
+
+            Opcode::Mov8 => todo!(),
+            Opcode::Mov16 => todo!(),
+            Opcode::Mov32 => todo!(),
+            Opcode::Mov64 => todo!(),
+            Opcode::MovR2R => todo!(),
+
+            Opcode::Add8 => todo!(),
+            Opcode::Add16 => todo!(),
+            Opcode::Add32 => todo!(),
+            Opcode::Add64 => todo!(),
+            Opcode::AddR2R => todo!(),
+            Opcode::XAdd => todo!(),
+            
+            Opcode::Sub8 => todo!(),
+            Opcode::Sub16 => todo!(),
+            Opcode::Sub32 => todo!(),
+            Opcode::Sub64 => todo!(),
+            Opcode::SubR2R => todo!(),
+
+            Opcode::Mul8 => todo!(),
+            Opcode::Mul16 => todo!(),
+            Opcode::Mul32 => todo!(),
+            Opcode::Mul64 => todo!(),
+            Opcode::MulR2R => todo!(),
+
+            Opcode::Div8 => todo!(),
+            Opcode::Div16 => todo!(),
+            Opcode::Div32 => todo!(),
+            Opcode::Div64 => todo!(),
+            Opcode::DivR2R => todo!(),
+
+            Opcode::Cmp8 => todo!(),
+            Opcode::Cmp16 => todo!(),
+            Opcode::Cmp32 => todo!(),
+            Opcode::Cmp64 => todo!(),
+            Opcode::CmpR2R => todo!(),
+
+            Opcode::Jmp => todo!(),
+            Opcode::Jz => todo!(),
+            Opcode::Jnz => todo!(),
+            Opcode::Je => todo!(),
+            Opcode::Jne => todo!(),
+        }
 
         Ok(())
     }
