77dcfef8723a9d157d9e4b7547c5b56249744a80
mealet <jasonragonter@gmail.com>
2025-12-12 22:05:40 +0300

    implemented lexer get_number function

--- src/assembly/error.rs
+++ src/assembly/error.rs
@@ -3,6 +3,10 @@ use thiserror::Error;
 
 use super::Source;
 
+pub fn position_to_span(from: usize, to: usize) -> SourceSpan {
+    (from, to.wrapping_sub(from)).into()
+}
+
 #[derive(Debug, Error, Diagnostic)]
 pub enum AssemblyError {
     #[error("Unknown character found: '{character}'")]
@@ -16,6 +20,35 @@ pub enum AssemblyError {
         #[source_code]
         src: Source,
         #[label("here")]
+        span: SourceSpan
+    },
+
+    #[error("Invalid number constant found")]
+    #[diagnostic(
+        severity(Error),
+        code(mvm::asm::invalid_number),
+    )]
+    InvalidNumberConstant {
+        const_type: String,
+
+        #[source_code]
+        src: Source,
+        #[label("constant type: {const_type}")]
+        span: SourceSpan
+    },
+
+    #[error("Number parser returned an error: {parser_error}")]
+    #[diagnostic(
+        severity(Error),
+        code(mvm::asm::invalid_number),
+    )]
+    ConstantParseError {
+        const_type: String,
+        parser_error: String,
+
+        #[source_code]
+        src: Source,
+        #[label("constant type: {const_type}")]
         span: SourceSpan
     }
 }
--- src/assembly/lexer/mod.rs
+++ src/assembly/lexer/mod.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 use miette::NamedSource;
 
-use super::{Source, error::AssemblyError};
+use super::{
+    Source,
+    error::{self, AssemblyError}
+};
 pub use token::{Token, TokenType};
 
 mod token;
@@ -127,6 +130,179 @@ impl Lexer {
     }
 }
 
+impl Lexer {
+    fn character_escape(escape: char) -> Option<char> {
+        match escape {
+            '0' => Some('\0'),
+            'n' => Some('\n'),
+            't' => Some('\t'),
+            'r' => Some('\r'),
+            '\\' => Some('\\'),
+            _ => None
+        }
+    }
+
+    fn get_number(&mut self) -> Result<Token, AssemblyError> {
+        #[derive(PartialEq, Debug)]
+        enum ParseMode {
+            Decimal,
+            Hexadecimal,
+            Binary,
+            Float
+        }
+
+        let mut value = String::new();
+        let mut mode = ParseMode::Decimal;
+        let span_start = self.position;
+
+        while self.peek_char().is_ascii_digit()
+            || self.peek_char().is_ascii_hexdigit()
+            || ['_', '.', 'x', 'b'].contains(&self.peek_char())
+        {
+            if self.peek_char() == '0' {
+                let after_zero = self.next_char();
+
+                match after_zero {
+                    'b' => {
+                        if mode != ParseMode::Decimal || !value.is_empty() {
+                            return Err(AssemblyError::InvalidNumberConstant {
+                                const_type: format!("{mode:?}").to_lowercase(),
+                                src: self.src.clone(),
+                                span: error::position_to_span(span_start, self.position)
+                            })
+                        }
+
+                        mode = ParseMode::Binary;
+                        let _ = self.next_char();
+                        continue;
+                    },
+
+                    'x' => {
+                        if mode != ParseMode::Decimal || !value.is_empty() {
+                            return Err(AssemblyError::InvalidNumberConstant {
+                                const_type: format!("{mode:?}").to_lowercase(),
+                                src: self.src.clone(),
+                                span: error::position_to_span(span_start, self.position)
+                            })
+                        }
+
+                        mode = ParseMode::Hexadecimal;
+                        let _ = self.next_char();
+                        continue;
+                    },
+
+                    _ => {
+                        value.push('0');
+                        continue;
+                    }
+                }
+            }
+
+            match self.peek_char() {
+                '_' => {},
+                '.' => {
+                    if mode != ParseMode::Decimal {
+                        return Err(AssemblyError::InvalidNumberConstant {
+                            const_type: format!("{mode:?}").to_lowercase(),
+                            src: self.src.clone(),
+                            span: error::position_to_span(span_start, self.position)
+                        });
+                    }
+
+                    mode = ParseMode::Float;
+                    value.push('.');
+                }
+
+                chr => value.push(chr)
+            }
+
+            let _ =self.next_char();
+        }
+
+        if value.is_empty() {
+            return Ok(Token::new(0.to_string(), TokenType::Constant, error::position_to_span(span_start, self.position)));
+        }
+
+        match mode {
+            ParseMode::Decimal => {
+                let result = value.trim().parse::<i64>();
+
+                if let Err(error) = result {
+                    return Err(AssemblyError::ConstantParseError {
+                        const_type: format!("{mode:?}").to_lowercase(),
+                        parser_error: error.to_string(),
+                        src: self.src.clone(),
+                        span: error::position_to_span(span_start, self.position)
+                    });
+                }
+
+                return Ok(Token::new(
+                    result.unwrap().to_string(),
+                    TokenType::Constant,
+                    error::position_to_span(span_start, self.position)
+                ));
+            }
+
+            ParseMode::Binary => {
+                let result = i64::from_str_radix(value.trim(), 2);
+
+                if let Err(error) = result {
+                    return Err(AssemblyError::ConstantParseError {
+                        const_type: format!("{mode:?}").to_lowercase(),
+                        parser_error: error.to_string(),
+                        src: self.src.clone(),
+                        span: error::position_to_span(span_start, self.position)
+                    });
+                }
+
+                return Ok(Token::new(
+                    result.unwrap().to_string(),
+                    TokenType::Constant,
+                    error::position_to_span(span_start, self.position)
+                ));
+            }
+
+            ParseMode::Hexadecimal => {
+                let result = i64::from_str_radix(value.trim(), 16);
+
+                if let Err(error) = result {
+                    return Err(AssemblyError::ConstantParseError {
+                        const_type: format!("{mode:?}").to_lowercase(),
+                        parser_error: error.to_string(),
+                        src: self.src.clone(),
+                        span: error::position_to_span(span_start, self.position)
+                    });
+                }
+
+                return Ok(Token::new(
+                    result.unwrap().to_string(),
+                    TokenType::Constant,
+                    error::position_to_span(span_start, self.position)
+                ));
+            }
+
+            ParseMode::Float => {
+                let result = value.trim().parse::<f64>();
+
+                if let Err(error) = result {
+                    return Err(AssemblyError::ConstantParseError {
+                        const_type: format!("{mode:?}").to_lowercase(),
+                        parser_error: error.to_string(),
+                        src: self.src.clone(),
+                        span: error::position_to_span(span_start, self.position)
+                    });
+                }
+
+                return Ok(Token::new(
+                    result.unwrap().to_string(),
+                    TokenType::Constant,
+                    error::position_to_span(span_start, self.position)
+                ));
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -146,5 +322,90 @@ mod tests {
 
         assert_eq!(lexer.next_char(), '\0');
         assert_eq!(lexer.peek_prev(), '3');
+    }
+
+    #[test]
+    fn lexer_get_number_basic_test() {
+        let mut lexer = Lexer::new("test", "123");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_ok());
+        
+        let number = number_result.unwrap();
+
+        assert_eq!(number.value, "123");
+        assert_eq!(number.token_type, TokenType::Constant);
+        assert_eq!(number.span.offset(), 0);
+        assert_eq!(number.span.len(), 3);
+    }
+
+    #[test]
+    fn lexer_get_number_binary_test() {
+        let mut lexer = Lexer::new("test", "0b1111");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_ok());
+        
+        let number = number_result.unwrap();
+
+        assert_eq!(number.value, "15");
+        assert_eq!(number.token_type, TokenType::Constant);
+        assert_eq!(number.span.offset(), 0);
+        assert_eq!(number.span.len(), 6);
+    }
+
+    #[test]
+    fn lexer_get_number_hexadecimal_test() {
+        let mut lexer = Lexer::new("test", "0xFF");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_ok());
+        
+        let number = number_result.unwrap();
+
+        assert_eq!(number.value, "255");
+        assert_eq!(number.token_type, TokenType::Constant);
+        assert_eq!(number.span.offset(), 0);
+        assert_eq!(number.span.len(), 4);
+    }
+
+    #[test]
+    fn lexer_get_number_float_test() {
+        let mut lexer = Lexer::new("test", "0.314000");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_ok());
+        
+        let number = number_result.unwrap();
+
+        assert_eq!(number.value, "0.314");
+        assert_eq!(number.token_type, TokenType::Constant);
+        assert_eq!(number.span.offset(), 0);
+        assert_eq!(number.span.len(), 8);
+    }
+
+
+    #[test]
+    fn lexer_get_number_error_1_test() {
+        let mut lexer = Lexer::new("test", "1.1.");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_err());
+    }
+
+    #[test]
+    fn lexer_get_number_error_2_test() {
+        let mut lexer = Lexer::new("test", "0bb");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_err());
+    }
+
+    #[test]
+    fn lexer_get_number_error_3_test() {
+        let mut lexer = Lexer::new("test", "0xx");
+        let number_result = lexer.get_number();
+
+        assert!(number_result.is_err());
     }
 }
