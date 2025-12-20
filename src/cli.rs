73ff00c23396ce096cd2f08def08091d04cb473c
mealet <jasonragonter@gmail.com>
2025-12-20 15:28:01 +0300

    implemented fancy cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,11 @@
 use clap::{arg, Command};
+use colored::Colorize;
 
 pub fn cli() -> Command {
     Command::new("mvm")
         .about("Mealet's 64-bit virtual machine")
+        .version(env!("CARGO_PKG_VERSION"))
+        .help_template("{options}")
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
@@ -18,17 +21,28 @@ pub fn cli() -> Command {
                         .default_value("128")
                         .required(false)
                 )
-                .arg(
-                    arg!(-l <LOG> "log machine to specified file")
-                        .required(false)
-                )
                 .arg(arg!(<PROGRAM> "path to program binary file"))
         )
         .subcommand(
             Command::new("compile")
                 .arg(
+                    arg!(-d --debug "debug build of program")
+                )
+                .arg(
                     arg!(<ASM> "assembly file path")
                 )
                 .arg_required_else_help(true)
         )
+}
+
+pub fn error(message: impl std::fmt::Display) {
+    eprintln!("{} {}", "Error:".red().bold(), message);
+}
+
+pub fn vm_error(message: impl std::fmt::Display) {
+    eprintln!("{} {}", "MVM PANIC:".red().bold(), message);
+}
+
+pub fn info(start: impl AsRef<str>, message: impl std::fmt::Display) {
+    println!("{} {}", start.as_ref().green().bold(), message);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,74 @@
-// TODO: Remove this when release
-#![allow(unused)]
+use colored::Colorize;
 
 mod cli;
 mod vm;
 mod assembly;
 
 fn main() {
-    // TODO: Implement fancy CLI
     // TODO: Add memory && stack size reader for MVM format
 
-    let cli = cli::cli().get_matches();
+    let cli = cli::cli().try_get_matches().unwrap_or_else(|e| {
+        let version = env!("CARGO_PKG_VERSION");
+        let authors_env = env!("CARGO_PKG_AUTHORS");
+        let authors_fmt = if authors_env.contains(":") {
+            format!("\n| {}", authors_env.replace(":", "\n| "))
+        } else {
+            authors_env.to_owned()
+        };
+
+        match e.kind() {
+            clap::error::ErrorKind::DisplayVersion => {
+                eprintln!("{}", "⚡ Mealet Virtual Machine".bold().red());
+                eprintln!("| - version: {version}");
+                eprintln!("| - authors: {authors_fmt}");
+
+                std::process::exit(0);
+            }
+
+            _ => {
+                let bin = env!("CARGO_PKG_NAME");
+
+                eprintln!("{}", "⚡ Mealet Virtual Machine".bold().red());
+                eprintln!("| - version: {version}");
+                eprintln!("| - authors: {authors_fmt}");
+                eprintln!("");
+                eprintln!("{}", "🍀 Options:".bold().red());
+
+                let _ = cli::cli().print_help();
+
+                eprintln!();
+                eprintln!("{}", "🎓 Examples of usage:".bold().red());
+                eprintln!("  {bin} compile hello_world.asm");
+                eprintln!("  {bin} run hello_world.mvm");
+                eprintln!("  {bin} run hello_world.mvm -m 1024 -s 256");
+
+                if e.kind() == clap::error::ErrorKind::DisplayHelp {
+                    std::process::exit(0);
+                }
+                std::process::exit(1);
+            }
+        }
+    });
 
     match cli.subcommand() {
         Some(("compile", sub_matches)) => {
+            cli::info("Reading", "source code");
+
             let path_to_asm = sub_matches.get_one::<String>("ASM").expect("asm path required");
-            let code = std::fs::read_to_string(path_to_asm).expect("unable to read");
+            let code = std::fs::read_to_string(path_to_asm).unwrap_or_else(|err| {
+                cli::error(format!("Unable to read assembly source code [{}]", err));
+                std::process::exit(1);
+            });
 
             let reporter = miette::GraphicalReportHandler::new();
+
+            cli::info("Compiling", format!("assembly file ({})", path_to_asm));
 
             let mut lexer = assembly::lexer::Lexer::new("TEST", &code);
             let tokens = lexer.tokenize().unwrap_or_else(|errors| {
                 for err in errors {
                     let mut buffer = String::new();
-                    reporter.render_report(&mut buffer, err);
+                    let _ = reporter.render_report(&mut buffer, err);
 
                     eprintln!("{}", buffer);
                 }
@@ -34,7 +80,7 @@ fn main() {
             let ast = parser.parse().unwrap_or_else(|errors| {
                 for err in errors {
                     let mut buffer = String::new();
-                    reporter.render_report(&mut buffer, err);
+                    let _ = reporter.render_report(&mut buffer, err);
 
                     eprintln!("{}", buffer);
                 }
@@ -46,7 +92,7 @@ fn main() {
             let _ = analyzer.analyze(&ast).unwrap_or_else(|errors| {
                 for err in errors {
                     let mut buffer = String::new();
-                    reporter.render_report(&mut buffer, err);
+                    let _ = reporter.render_report(&mut buffer, err);
 
                     eprintln!("{}", buffer);
                 }
@@ -57,16 +103,20 @@ fn main() {
             let mut codegen = assembly::codegen::Codegen::new();
             let code = codegen.compile(&ast);
 
-            std::fs::write(path_to_asm.replace(".asm", ".mvm"), code);
+            cli::info("Writing", "generated code to binary");
+
+            let new_file = path_to_asm.replace(".asm", ".mvm");
+
+            std::fs::write(&new_file, code).unwrap_or_else(|err| {
+                cli::error(format!("Unable to write generated code to binary file [{}]", err));
+            });
+
+            cli::info("Successfully", format!("compiled assembly to mvm binary: {}", new_file));
         },
 
         Some(("run", sub_matches)) => {
             let memsize = sub_matches.get_one::<String>("MEMSIZE").expect("no memsize found");
             let stacksize = sub_matches.get_one::<String>("STACKSIZE").expect("no stacksize found");
-            let log_mode = match sub_matches.get_one::<String>("LOG") {
-                Some(str) => str == "true",
-                _ => false
-            };
 
             let program_path = sub_matches.get_one::<String>("PROGRAM").expect("no program path found");
             let program = std::fs::read(program_path).expect("unable to read program");
@@ -75,16 +125,18 @@ fn main() {
             let stacksize = stacksize.parse::<usize>().expect("unable to parse memsize");
 
             let mut vm = vm::VM::new(memsize, stacksize).unwrap_or_else(|err| {
-                eprintln!("{}", err);
+                cli::error(format!("Unable to create VM instance [{}]", err));
                 std::process::exit(1);
             });
 
-            vm.insert_program(&program);
-            let result = vm.run();
+            vm.insert_program(&program).unwrap_or_else(|err| {
+                cli::error(format!("Unable to load the program [{}]", err));
+            });
 
-            if result.is_err() {
-                println!("{}", result.unwrap_err());
-            }
+            let _ = vm.run().unwrap_or_else(|err| {
+                cli::vm_error(err);
+                std::process::exit(1);
+            });
         }
 
         _ => unreachable!()
