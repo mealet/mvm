d2446d59ae3ea3159df490feade0066729fd94af
mealet <jasonragonter@gmail.com>
2026-01-02 23:27:25 +0300

    updated hello world example

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -10,21 +10,38 @@ section .data
 section .text
 entry _start
 
-_start:
+; ===| Functions |===
+
+print:
   ; -- syscall void write(int output, void *buffer, size_t length) --
 
-  mov %r0, $1 ; int output (stdout)
+  mov %r2, %r1 ; size_t length (length variable)
 
-  mov %r1, hello ; void* buffer (ptr to message)
-  mov %r2, len ; size_t length (length variable)
+  mov %r1, %r0 ; void* buffer (ptr to message)
+  add %r1, $8 ; string address offset
+
+  mov %r0, $1 ; int output (stdout)
 
   mov %call, $2 ; `write` syscall
   int $syscall ; system call interrupt
 
-  jmp exit
+  ret
 
 exit:
   ; -- syscall void exit(int status) --
+
   mov %r0, $0 ; int status
   mov %call, $0 ; `exit` syscall
+
   int $syscall ; system call interrupt
+
+  ret
+
+; ===| Program Entrypoint |===
+
+_start:
+  mov %r0, hello
+  mov %r1, len
+
+  call print
+  call exit
